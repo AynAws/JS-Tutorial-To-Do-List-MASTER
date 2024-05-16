e9e3bc4ecfe53b8c7d52d7785a75fc1823fde119
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-05-16 09:30:12 -0400

    done, line 22 broke

--- js/app.js
+++ js/app.js
@@ -14,6 +14,31 @@ const LINE_THROUGH = 'lineThrough';
 // Var
 let LIST, id;
 
+// get item from local storage
+let data = localStorage.getItem('TODO');
+
+// check if data is not empty
+if (data) {
+    LIST = JSON.parse(data); // BROKEN
+    id = LIST.length; // set id to last list item
+    loadList(LIST); // load list to UI
+} else {
+    // if data empty
+    LIST = [];
+    id = 0;
+}
+function loadList(array) {
+    array.forEach(function(item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    })
+};
+
+// clear local storage
+clear.addEventListener('click', function() {
+    localStorage.clear();
+    location.reload();
+});
+
 // Show todays date
 const options = {weekday: 'long', month: 'short', day:'numeric'}
 const today = new Date();
@@ -43,10 +68,45 @@ document.addEventListener('keyup', function(even) {
         const toDo = input.value;
         // if not empty
         if (toDo) {
-            addToDo(toDo);
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            })
+            // add item to local storage
+            localStorage.setItem('TODO', JSON.stringify(LIST));
+            id++;
         }
         input.value = ''
     }
 });
 
-addToDo('Coffee', 1, false, true)
+// complete toDo
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH);
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+};
+
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+};
+
+// taregt the items created dynamically
+
+list.addEventListener('click', function(event){
+    const element = event.target; // returns element clicked
+    const elementJob = element.attributes.job.value; // complete/delete
+
+    if (elementJob == 'complete') {
+        completeToDo(element);
+    } else if (elementJob == 'delete') {
+        removeToDo(element);
+    }
+});
